55c4bf8fc3a66809797954f6db6f5db250056415
Kama MacPro <macbook@MacBook-Pro.local>
2022-06-22 14:24:14 +0200

    fixed

---
+++ temp/js/main.js
@@ -0,0 +1,9 @@
+var mixer = mixitup('.container_tabs');
+var mixer = mixitup(containerEl, {
+    selectors: {
+        target: '.blog-item'
+    },
+    animation: {
+        duration: 300
+    }
+});
