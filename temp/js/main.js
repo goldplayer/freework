51fbf31f2ff443f90f528aa50f8201389e930564
KostyaKaraman <kostya.karaman44@gmail.com>
2022-07-05 22:16:51 +0300

    fixed

--- temp/js/main.js
+++ temp/js/main.js
@@ -1,5 +1,13 @@
-// MIXITUP PLUGINS
-$(document).ready(function(){
-    var mixer = mixitup('.box-list')
-});
-// 
+// // MIXITUP PLUGINS
+// $(document).ready(function(){
+//     var mixer = mixitup('.box-list')
+// });
+// // 
+
+
+let menuBtn = document.querySelector('.menu_btn');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
